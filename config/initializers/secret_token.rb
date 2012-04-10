57ccd68d94712b25bd3474e1cfd58eb18a624c43
unknown <0503313@CL56-061.vsn.co.jp>
2012-04-10 17:32:17 +0900

    Test Update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VsnQuestionnaireProject::Application.config.secret_token = '360f9a62a4a59e4acd03c8ea4fadc61a9097e58b47ee8043540781f9f7e8b9c289d13da84df94ba50ba0ac49b63be65a8a192ac727b2dd5e9c834ad7ec009987'
